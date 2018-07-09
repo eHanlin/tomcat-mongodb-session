db36cd6f333373c9a877a0df35222b7abf4e3950
hotdog <hotdog929@gmail.com>
2018-07-09 16:48:23 +0800

    session expire not trigger sync

--- src/main/java/tw/com/ehanlin/tomcatMongodbSession/MongoSession.java
+++ src/main/java/tw/com/ehanlin/tomcatMongodbSession/MongoSession.java
@@ -3,6 +3,10 @@ package tw.com.ehanlin.tomcatMongodbSession;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.FindOneAndUpdateOptions;
 import com.mongodb.client.model.UpdateOptions;
+import org.apache.catalina.Context;
+import org.apache.catalina.Globals;
+import org.apache.catalina.TomcatPrincipal;
+import org.apache.tomcat.util.ExceptionUtils;
 import org.bson.Document;
 
 import org.apache.catalina.Manager;
@@ -10,10 +14,11 @@ import org.apache.catalina.session.StandardSession;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
 
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
 import java.util.regex.Pattern;
 
 import static com.mongodb.client.model.Filters.eq;
-import static com.mongodb.client.model.Updates.*;
 
 final public class MongoSession extends StandardSession {
 
@@ -65,8 +70,15 @@ final public class MongoSession extends StandardSession {
     @Override
     protected void removeAttributeInternal(String name, boolean notify) {
         //log.info("removeAttributeInternal id=" + this.id);
+        this.removeAttributeInternal(name, notify, true);
+    }
+
+    protected void removeAttributeInternal(String name, boolean notify, boolean sync) {
+        //log.info("removeAttributeInternal id=" + this.id);
         super.removeAttributeInternal(name, notify);
-        syncRemoveOneAttribute(name);
+        if(sync){
+            syncRemoveOneAttribute(name);
+        }
     }
 
     private void syncFromMongo() {
@@ -108,6 +120,92 @@ final public class MongoSession extends StandardSession {
                 eq(SESSION_ID, this.id),
                 new Document("$set", new Document(LAST_UPDATE_DATE, System.currentTimeMillis()))
                     .append("$unset", new Document(name, "")));
+        }
+    }
+
+    @Override
+    public void expire(boolean notify) {
+        //log.info("expire id=" + this.id);
+        if (this.isValid) {
+            synchronized(this) {
+                if (!this.expiring && this.isValid) {
+                    if (this.manager != null) {
+                        this.expiring = true;
+                        Context context = this.manager.getContext();
+                        if (notify) {
+                            ClassLoader oldContextClassLoader = null;
+
+                            try {
+                                oldContextClassLoader = context.bind(Globals.IS_SECURITY_ENABLED, (ClassLoader)null);
+                                Object[] listeners = context.getApplicationLifecycleListeners();
+                                if (listeners != null && listeners.length > 0) {
+                                    HttpSessionEvent event = new HttpSessionEvent(this.getSession());
+
+                                    for(int i = 0; i < listeners.length; ++i) {
+                                        int j = listeners.length - 1 - i;
+                                        if (listeners[j] instanceof HttpSessionListener) {
+                                            HttpSessionListener listener = (HttpSessionListener)listeners[j];
+
+                                            try {
+                                                context.fireContainerEvent("beforeSessionDestroyed", listener);
+                                                listener.sessionDestroyed(event);
+                                                context.fireContainerEvent("afterSessionDestroyed", listener);
+                                            } catch (Throwable var29) {
+                                                ExceptionUtils.handleThrowable(var29);
+
+                                                try {
+                                                    context.fireContainerEvent("afterSessionDestroyed", listener);
+                                                } catch (Exception var28) {
+                                                    ;
+                                                }
+
+                                                this.manager.getContext().getLogger().error(sm.getString("standardSession.sessionEvent"), var29);
+                                            }
+                                        }
+                                    }
+                                }
+                            } finally {
+                                context.unbind(Globals.IS_SECURITY_ENABLED, oldContextClassLoader);
+                            }
+                        }
+
+                        if (ACTIVITY_CHECK) {
+                            this.accessCount.set(0);
+                        }
+
+                        this.manager.remove(this, true);
+                        if (notify) {
+                            this.fireSessionEvent("destroySession", (Object)null);
+                        }
+
+                        if (this.principal instanceof TomcatPrincipal) {
+                            TomcatPrincipal gp = (TomcatPrincipal)this.principal;
+
+                            try {
+                                gp.logout();
+                            } catch (Exception var27) {
+                                this.manager.getContext().getLogger().error(sm.getString("standardSession.logoutfail"), var27);
+                            }
+                        }
+
+                        this.setValid(false);
+                        this.expiring = false;
+                        String[] keys = this.keys();
+                        ClassLoader oldContextClassLoader = null;
+
+                        try {
+                            oldContextClassLoader = context.bind(Globals.IS_SECURITY_ENABLED, (ClassLoader)null);
+
+                            for(int i = 0; i < keys.length; ++i) {
+                                this.removeAttributeInternal(keys[i], notify, false);
+                            }
+                        } finally {
+                            context.unbind(Globals.IS_SECURITY_ENABLED, oldContextClassLoader);
+                        }
+
+                    }
+                }
+            }
         }
     }
 }
